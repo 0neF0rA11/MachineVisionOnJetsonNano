36d9081b188193fa485da04c78abfbb00612635a
Alexander <sanya.oleinickov22@yandex.ru>
2024-10-21 13:31:50 +0300

    Added server logging

--- server.py
+++ server.py
@@ -1,6 +1,25 @@
 import time
 import serial
-import serial.tools.list_ports
+import logging
+import os
+
+
+LOG_FILE = 'server_log.txt'
+MAX_LOG_LINES = 100
+
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def manage_log_file():
+    """Удаление старых строк, если количество строк в логе превышает MAX_LOG_LINES."""
+    if os.path.exists(LOG_FILE):
+        with open(LOG_FILE, 'r') as f:
+            lines = f.readlines()
+
+        if len(lines) > MAX_LOG_LINES:
+            with open(LOG_FILE, 'w') as f:
+                f.writelines(lines[-MAX_LOG_LINES:])
 
 
 class Server:
@@ -9,7 +28,7 @@ class Server:
         self.ser = None
         self.ports = None
 
-    def connect_port(self, page, received_label):
+    def connect_port(self, page):
         try:
             self.ser = serial.Serial(
                 port="/dev/ttyTHS1",
@@ -19,28 +38,40 @@ class Server:
                 stopbits=serial.STOPBITS_ONE,
             )
             time.sleep(1)
-            self.read_from_port(page, received_label)
+            logging.info("Подключение к порту /dev/ttyTHS1 успешно установлено.")
+            self.read_from_port(page)
         except Exception as e:
-            print(f"Error: {e}")
+            logging.error(f"Ошибка при подключении к порту: {e}")
+        manage_log_file()
 
-    def close_port(self, label):
+    def close_port(self):
         if self.ser and self.ser.is_open:
             self.ser.close()
-            # label.config(text="Port Closed")
-        # else:
-        #     label.config(text="No Port is Open")
+            logging.info("Порт был закрыт.")
+        else:
+            logging.warning("Попытка закрыть порт, который не был открыт.")
+        manage_log_file()
 
-    def read_from_port(self, page, received_label):
+    def read_from_port(self, page):
         if self.ser and self.ser.is_open:
             if self.ser.in_waiting > 0:
-                self.received_data = self.ser.read(self.ser.in_waiting).decode('utf-8', errors='replace').strip()
-                received_label.config(text=f"Received: {self.received_data}")
-                page.response_to_request(self.received_data.split())
-            page.after(1, lambda: self.read_from_port(page, received_label))
+                try:
+                    self.received_data = self.ser.read(self.ser.in_waiting).decode('utf-8', errors='replace').strip()
+                    logging.info(f"Получены данные: {self.received_data}")
+                    page.response_to_request(self.received_data.split())
+                except Exception as e:
+                    logging.error(f"Ошибка при чтении данных: {e}")
+                manage_log_file()
+            page.after(1, lambda: self.read_from_port(page))
 
-    def send_command(self, command, label):
+    def send_command(self, command):
         if self.ser and self.ser.is_open:
-            self.ser.write(command.encode())
-            time.sleep(0.002)
-            label.config(text=f"Sent: {command.strip()}")
-
+            try:
+                self.ser.write(command.encode())
+                time.sleep(0.002)
+                logging.info(f"Отправлены данные: {command.strip()}")
+            except Exception as e:
+                logging.error(f"Ошибка при отправке команды: {e}")
+        else:
+            logging.warning("Попытка отправки команды через закрытый порт.")
+        manage_log_file()
